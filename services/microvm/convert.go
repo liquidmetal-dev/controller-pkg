78ff4db3b65cea9f596102b5131306ec416190b6
Claudia Beresford <claudiaberesford@gmail.com>
2022-11-17 12:15:26 +0000

    feat: Extract flintlock service

---
+++ services/microvm/convert.go
@@ -0,0 +1,85 @@
+// Copyright 2022 Weaveworks or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MPL-2.0
+
+package microvm
+
+import (
+	types "github.com/weaveworks-liquidmetal/controller-pkg/types/microvm"
+	flintlocktypes "github.com/weaveworks-liquidmetal/flintlock/api/types"
+)
+
+const platformLiquidMetal = "liquid_metal"
+
+func convertToFlintlockAPI(mvmScope Scope) *flintlocktypes.MicroVMSpec {
+	mvmSpec := mvmScope.GetMicrovmSpec()
+
+	apiVM := &flintlocktypes.MicroVMSpec{
+		Id:         mvmScope.Name(),
+		Namespace:  mvmScope.Namespace(),
+		Vcpu:       int32(mvmSpec.VCPU),
+		MemoryInMb: int32(mvmSpec.MemoryMb),
+		Kernel: &flintlocktypes.Kernel{
+			Image:            mvmSpec.Kernel.Image,
+			Cmdline:          mvmSpec.KernelCmdLine,
+			AddNetworkConfig: true,
+			Filename:         &mvmSpec.Kernel.Filename,
+		},
+		RootVolume: &flintlocktypes.Volume{
+			Id:         "root",
+			IsReadOnly: mvmSpec.RootVolume.ReadOnly,
+			Source: &flintlocktypes.VolumeSource{
+				ContainerSource: &mvmSpec.RootVolume.Image,
+			},
+		},
+		Metadata: map[string]string{},
+	}
+
+	if mvmSpec.Initrd != nil {
+		apiVM.Initrd = &flintlocktypes.Initrd{
+			Image:    mvmSpec.Initrd.Image,
+			Filename: &mvmSpec.Initrd.Filename,
+		}
+	}
+
+	apiVM.AdditionalVolumes = []*flintlocktypes.Volume{}
+
+	for i := range mvmSpec.AdditionalVolumes {
+		volume := mvmSpec.AdditionalVolumes[i]
+
+		apiVM.AdditionalVolumes = append(apiVM.AdditionalVolumes, &flintlocktypes.Volume{
+			Id:         volume.ID,
+			IsReadOnly: volume.ReadOnly,
+			Source: &flintlocktypes.VolumeSource{
+				ContainerSource: &volume.Image,
+			},
+		})
+	}
+
+	apiVM.Interfaces = []*flintlocktypes.NetworkInterface{}
+
+	for i := range mvmSpec.NetworkInterfaces {
+		iface := mvmSpec.NetworkInterfaces[i]
+
+		apiIface := &flintlocktypes.NetworkInterface{
+			DeviceId: iface.GuestDeviceName,
+			GuestMac: &iface.GuestMAC,
+		}
+
+		if iface.Address != "" {
+			apiIface.Address = &flintlocktypes.StaticAddress{
+				Address: iface.Address,
+			}
+		}
+
+		switch iface.Type {
+		case types.IfaceTypeMacvtap:
+			apiIface.Type = flintlocktypes.NetworkInterface_MACVTAP
+		case types.IfaceTypeTap:
+			apiIface.Type = flintlocktypes.NetworkInterface_TAP
+		}
+
+		apiVM.Interfaces = append(apiVM.Interfaces, apiIface)
+	}
+
+	return apiVM
+}
---
+++ services/microvm/service.go
@@ -0,0 +1,178 @@
+// Copyright 2022 Weaveworks or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MPL-2.0
+
+package microvm
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+
+	flclient "github.com/weaveworks-liquidmetal/controller-pkg/client"
+	microvm "github.com/weaveworks-liquidmetal/controller-pkg/types/microvm"
+	flintlockv1 "github.com/weaveworks-liquidmetal/flintlock/api/services/microvm/v1alpha1"
+	flintlocktypes "github.com/weaveworks-liquidmetal/flintlock/api/types"
+	"github.com/weaveworks-liquidmetal/flintlock/client/cloudinit/instance"
+	"github.com/weaveworks-liquidmetal/flintlock/client/cloudinit/userdata"
+	"github.com/yitsushi/macpot"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"gopkg.in/yaml.v2"
+	"k8s.io/utils/pointer"
+)
+
+const (
+	cloudInitHeader = "#cloud-config\n"
+)
+
+// Scope contains functions for operators to provide microvm request  configuration
+// to the service.
+type Scope interface {
+	// Name returns the kubernetes name of the object creating the microvm.
+	Name() string
+	// Namespace returns the kubernetes namespace of the object creating the microvm.
+	Namespace() string
+	// GetMicrovmSpec returns the full spec as configured by the calling operator.
+	GetMicrovmSpec() microvm.VMSpec
+	// GetInstanceID returns the UUID of the microvm.
+	GetInstanceID() string
+	// GetRawBootstrapData returns customised commands/cloud init to be run at microvm boot.
+	GetRawBootstrapData() string
+	// GetSSHPublicKeys returns the public keys to be added to the microvm.
+	GetSSHPublicKeys() []microvm.SSHPublicKey
+}
+
+type Service struct {
+	scope Scope
+
+	client flclient.Client
+	hostID string
+}
+
+func New(scope Scope, client flclient.Client, hostID string) *Service {
+	return &Service{
+		scope:  scope,
+		client: client,
+		hostID: hostID,
+	}
+}
+
+func (s *Service) Create(ctx context.Context) (*flintlocktypes.MicroVM, error) {
+	apiMicroVM := convertToFlintlockAPI(s.scope)
+
+	if err := s.addMetadata(apiMicroVM); err != nil {
+		return nil, fmt.Errorf("adding metadata: %w", err)
+	}
+
+	for i := range apiMicroVM.Interfaces {
+		iface := apiMicroVM.Interfaces[i]
+
+		if iface.GuestMac == nil || *iface.GuestMac == "" {
+			mac, err := macpot.New(macpot.AsLocal(), macpot.AsUnicast())
+			if err != nil {
+				return nil, fmt.Errorf("creating mac address client: %w", err)
+			}
+
+			iface.GuestMac = pointer.String(mac.ToString())
+		}
+	}
+
+	input := &flintlockv1.CreateMicroVMRequest{
+		Microvm: apiMicroVM,
+	}
+
+	resp, err := s.client.CreateMicroVM(ctx, input)
+	if err != nil {
+		return nil, fmt.Errorf("creating microvm: %w", err)
+	}
+
+	return resp.Microvm, nil
+}
+
+func (s *Service) Get(ctx context.Context) (*flintlocktypes.MicroVM, error) {
+	input := &flintlockv1.GetMicroVMRequest{
+		Uid: s.scope.GetInstanceID(),
+	}
+
+	resp, err := s.client.GetMicroVM(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Microvm, nil
+}
+
+func (s *Service) Delete(ctx context.Context) (*emptypb.Empty, error) {
+	input := &flintlockv1.DeleteMicroVMRequest{
+		Uid: s.scope.GetInstanceID(),
+	}
+
+	return s.client.DeleteMicroVM(ctx, input)
+}
+
+func (s *Service) Close() {
+	s.client.Close()
+}
+
+func (s *Service) addMetadata(apiMicroVM *flintlocktypes.MicroVMSpec) error {
+	apiMicroVM.Metadata["user-data"] = s.scope.GetRawBootstrapData()
+
+	vendorData, err := s.createVendorData()
+	if err != nil {
+		return fmt.Errorf("creating vendor data for microvm: %w", err)
+	}
+
+	apiMicroVM.Metadata["vendor-data"] = vendorData
+
+	instanceData, err := s.createInstanceData()
+	if err != nil {
+		return fmt.Errorf("creating instance metadata: %w", err)
+	}
+
+	apiMicroVM.Metadata["meta-data"] = instanceData
+
+	return nil
+}
+
+func (s *Service) createVendorData() (string, error) {
+	// TODO: remove the boot command temporary fix after image-builder change #89
+	vendorUserdata := &userdata.UserData{
+		HostName:     s.scope.Name(),
+		FinalMessage: "The Liquid Metal booted system is good to go after $UPTIME seconds",
+		BootCommands: []string{
+			"ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf",
+		},
+	}
+
+	for _, key := range s.scope.GetSSHPublicKeys() {
+		user := userdata.User{
+			Name:              key.User,
+			SSHAuthorizedKeys: key.AuthorizedKeys,
+		}
+
+		vendorUserdata.Users = append(vendorUserdata.Users, user)
+	}
+
+	data, err := yaml.Marshal(vendorUserdata)
+	if err != nil {
+		return "", fmt.Errorf("marshalling bootstrap data: %w", err)
+	}
+
+	dataWithHeader := append([]byte(cloudInitHeader), data...)
+
+	return base64.StdEncoding.EncodeToString(dataWithHeader), nil
+}
+
+func (s *Service) createInstanceData() (string, error) {
+	userMetadata := instance.New(
+		instance.WithLocalHostname(s.scope.Name()),
+		instance.WithPlatform(platformLiquidMetal),
+		instance.WithKeyValue("vm_host", s.hostID),
+	)
+
+	userMeta, err := yaml.Marshal(userMetadata)
+	if err != nil {
+		return "", fmt.Errorf("unable to marshal metadata: %w", err)
+	}
+
+	return base64.StdEncoding.EncodeToString(userMeta), nil
+}
